ef01613478139f95f5122a8a37e0468e9be3ad54
syuzo <syuzoonwr7000@gmail.com>
2025-03-17 12:56:27 +0900

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Hello from Vue.js!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
