e76853ec6d228bca8f0d1dacdce6f84b2a293595
syuzo <syuzoonwr7000@gmail.com>
2025-03-17 15:59:38 +0900

    fastAPIとVue.js連携

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI!"}
+# Vue.js のビルド後の静的ファイルを提供
+app.mount("/", StaticFiles(directory="frontend/dist", html=True), name="static")
